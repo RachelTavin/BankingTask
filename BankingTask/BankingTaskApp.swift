abc63fb9f3552b4abf013efe2a30b559eb0608f0
Racheli Tavin <rt9743113@gmail.com>
2026-05-20 13:42:09 +0300

    Initial Commit

---
+++ BankingTask/BankingTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankingTaskApp.swift
+//  BankingTask
+//
+//  Created by TavinFam on 20/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct BankingTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
